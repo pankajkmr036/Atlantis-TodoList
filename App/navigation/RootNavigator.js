bf2bc1dfa7699ec7f2cd4680d5c8a02db6a2f259
Pankaj Kumar <pankajk@orowealth.com>
2021-10-15 18:09:21 +0530

    Added custom Navigation bar for stack navigator

--- App/navigation/RootNavigator.js
+++ App/navigation/RootNavigator.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LandingScreen from '../screens/LandingScreen';
 
+import CustomNavigationBar from './CustomNavigationBar';
+import LandingScreen from '../screens/LandingScreen';
 import TaskViewScreen from '../screens/TaskViewScreen';
 import AddTaskScreen from '../screens/AddTaskScreen';
 
@@ -12,7 +13,10 @@ const RootNavigator = ({initialRouteName}) => (
   <NavigationContainer>
     <Stack.Navigator
       initialRouteName={initialRouteName}
-      screenOptions={{gestureEnabled: false, headerShown: false}}>
+      screenOptions={{
+        gestureEnabled: false,
+        header: props => <CustomNavigationBar {...props} />,
+      }}>
       <Stack.Screen name="LandingScreen" component={LandingScreen} />
       <Stack.Screen name="TaskViewScreen" component={TaskViewScreen} />
       <Stack.Screen name="AddTaskScreen" component={AddTaskScreen} />
