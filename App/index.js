d64faeb8068c14f158ebe96fd99d7b76ac76f179
Pankaj Kumar <pankajk@orowealth.com>
2021-10-15 17:51:49 +0530

    Wrapped Redux store and Redux Persist gate component at top level of app

--- App/index.js
+++ App/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
-import RootNavigator from './navigation/RootNavigator';
 import {Provider as PaperProvider} from 'react-native-paper';
+import {Provider as StoreProvider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {store, persistor} from './redux/store';
+import RootNavigator from './navigation/RootNavigator';
 
 const App = () => {
   return (
-    <PaperProvider>
-      <RootNavigator initialRouteName="LandingScreen" />
-    </PaperProvider>
+    <StoreProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <PaperProvider>
+          <RootNavigator initialRouteName="LandingScreen" />
+        </PaperProvider>
+      </PersistGate>
+    </StoreProvider>
   );
 };
 
